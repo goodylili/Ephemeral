0fcf4a0bcaee8834814b05f8d688d72bad68685b
Goodnessuc <ukejegoodness599@gmail.com>
2023-09-26 14:23:31 +0100

    [add]: added prelimary files

---
+++ internal/transport/websockets/upgrader.go
@@ -0,0 +1,12 @@
+package websockets
+
+import (
+	"github.com/gorilla/websocket"
+	"net/http"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
