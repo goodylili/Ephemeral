f4393c40fb349ae21c739de0ac39992654c31033
Goodnessuc <ukejegoodness599@gmail.com>
2023-10-14 12:50:14 +0100

    [add]: added chatroom creation and joining functionalityt

--- internal/transport/http/chatroom.go
+++ internal/transport/http/chatroom.go
@@ -1,9 +1,40 @@
 package http
 
 import (
+	"io/ioutil"
 	"net/http"
 )
 
-func ChatPageHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "path_to_your_html_file.html")
+func handleJoinChatRoomForm(writer http.ResponseWriter, request *http.Request) (string, string, error) {
+	err := request.ParseForm()
+	if err != nil {
+		return "", "", err
+	}
+	username := request.FormValue("username")
+	chatroomId := request.FormValue("chatroomId")
+
+	_, err = ioutil.ReadFile("../../static/html/join.html") // Assuming data.txt is the file you want to read.
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return "", "", err
+	}
+
+	return username, chatroomId, nil
+
+}
+
+func handleCreateChatRoomForm(writer http.ResponseWriter, request *http.Request) (string, string, error) {
+	err := request.ParseForm()
+	if err != nil {
+		return "", "", err
+	}
+	username := request.FormValue("username")
+	roomname := request.FormValue("roomname")
+
+	_, err = ioutil.ReadFile("../../static/html/login.html") // Assuming data.txt is the file you want to read.
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return "", "", err
+	}
+	return username, roomname, nil
 }
